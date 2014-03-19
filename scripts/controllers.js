92077d31586a6baa3b8c4c454a3d6da196326fb1
Axel Samuelsson <axelsam@kth.se>
2014-03-19 16:02:35 +0100

    LE SEARCH

--- scripts/controllers.js
+++ scripts/controllers.js
@@ -10,7 +10,11 @@ $(document).ready(function () {
 
 function createMiddleView(type) {
 	if(type === 'search') {
-		$('#midpane').html('<div class="row"><div id="searchfield"><input type="text"></div></div><div class="row"><div id="searchresults"></div></div>');
+		$('#midpane').html('<div class="row"><div id="searchFieldArea"><input id="searchField" type="text"></div></div><div class="row"><div id="searchresults"></div></div>');
+		$('#searchField').keyup(function () {
+			console.log('le search');
+			//REMEMBER TOLOWERCASE AMAZING SEARCH ALGORITHM
+		});
 	}
 	else if(type === 'browse') {
 		$('#midpane').html('browse lol');
