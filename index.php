bbdc1dec581b63d82f15594595f9f8e6b1b35386
Axel Samuelsson <axelsam@kth.se>
2014-03-19 18:37:56 +0100

    layout overhaul

--- index.php
+++ index.php
@@ -6,20 +6,81 @@
     <title>SÅNGBOK</title>
 
     <!-- Bootstrap CSS -->
-    <link rel="stylesheet" href="http://netdna.bootstrapcdn.com/bootstrap/3.1.0/css/bootstrap.min.css">
     <link rel="stylesheet" href="resources/style.css">
 
     <?php echo '<!--bahjs-->'; ?>
 </head>
 <!-- MAIN VIEW -->
-<body style="height: 100%">
-    <!-- Left pane -->
-    <div id="leftpane" class="container col-md-3" style="background-color: #c7ffde; height: 100%">
+<body>
+    <table id="page">
+        <tbody>
+            <tr>
+                <!-- TOP LEFT -->
+                <td id="a1">
+                    <button id="searchBtn">SEARCH</button><br>
+                    <button id="browseBtn">BROWSE</button><br><br>
+                    <span id="user">Jesper Bratt</span><br>
+                    <span id="user_thing">MEGA POWER</span>
+                    <hr>
+                </td>
+                <!-- TOP MIDDLE -->
+                <td id="a2">
+                    <div id="searchFieldArea">
+                        <input id="searchField" type="text">
+                    </div>
+                    <div id="selectSearchArea">
+                        <form>
+                            <input type="radio" name="type" id="songs" value="songs" checked><label for="songs">SONGS</label> | 
+                            <input type="radio" name="type" id="users" value="users"><label for="users">USERS</label> | 
+                            <input type="radio" name="type" id="collections" value="collections"><label for="collections">COLLECTIONS</label>
+                        </form>
+                    </div>
+                    <hr>
+                </td>
+                <!-- TOP RIGHT -->
+                <td id="a3">
+                    <div>lol</div>
+                    <hr>
+                </td>
+            </tr>
+            <tr>
+                <!-- MIDDLE LEFT -->
+                <td id="b1">
+                    
+                </td>
+                <!-- MIDDLE MIDDLE -->
+                <td id="b2">
+                    
+                </td>
+                <!-- MIDDLE RIGHT -->
+                <td id="b3">
+                    
+                </td>
+            </tr>
+            <tr>
+                <!-- BOTTOM LEFT -->
+                <td id="c1">
+                    
+                </td>
+                <!-- BOTTOM MIDDLE -->
+                <td id="c2">
+                    
+                </td>
+                <!-- BOTTOM RIGHT -->
+                <td id="c3">
+                    
+                </td>
+            </tr>
+        </tbody>
+    </table>
+    <!-- 
+
+    <div id="leftpane">
         <div id="upperLeft" style="border-bottom: solid #b25c56; border-bottom-width: 1px">
             
-                <div class="row" style="text-align: right"><button id="searchBtn" class="btn btn-default" style="margin-bottom: 4px; background-color: #ffb3ae; color: #b25c56;">Search</button></div> <!--float: right-->
-                <div class="row" style="text-align: right"><button id="browseBtn" class="btn btn-default" style="margin-bottom: 4px; background-color: #68b287; color: #c7ffde;">Browse</button></div>
-                <div class="row" style="text-align: right"><h5 style="color: #ffb3ae; margin-bottom: 0px; margin-right: 15px">User</h5></div> <!--text-align: right-->
+                <div class="row" style="text-align: right">Search</button></div>
+                <div class="row" style="text-align: right"><button id="browseBtn" class="btn btn-default" style="margin-bottom: 4px; ">Browse</button></div>
+                <div class="row" style="text-align: right"><h5 style="color: #ffb3ae; margin-bottom: 0px; margin-right: 15px">User</h5></div>
                 <div class="row" style="text-align: right"><h4 style="color: #b25c56; text-transform: uppercase; margin-top: 1px; margin-right: 15px">Username</h4></div>
         </div>
         <div id="middleLeft">
@@ -30,7 +91,6 @@
         </div>
     </div>
 
-    <!-- Middle pane -->
     <div id="midpane" class="container col-md-6" style="background-color: #ffb3ae; height: 100%">
         <div id="search" class="row">
             <div id="searchFieldArea">
@@ -38,9 +98,9 @@
             </div>
             <div id="selectSearchArea">
                 <form>
-                    <input type="radio" name="type" id="songs" value="songs"><label for="songs">Songs</label>
-                    <input type="radio" name="type" id="users" value="users"><label for="users">Users</label>
-                    <input type="radio" name="type" id="collections" value="collections"><label for="collections">Collections</label>
+                    <input type="radio" name="type" id="songs" value="songs" checked><label for="songs">SONGS</label> | 
+                    <input type="radio" name="type" id="users" value="users"><label for="users">USERS</label> | 
+                    <input type="radio" name="type" id="collections" value="collections"><label for="collections">COLLECTIONS</label>
                 </form>
             </div>
             <div id="searchresults">
@@ -51,7 +111,6 @@
         </div>
     </div>
 
-    <!-- Right pane -->
     <div id="rightpane" class="container col-md-3" style="background-color: #c7ffde; height: 100%">
         <div id="upperRight" style="border-bottom: solid #b25c56; border-bottom-width: 1px">
             <h5 style="color: #ffb3ae;">Current collection</h4>
@@ -64,7 +123,7 @@
 
         </div>
 
-    </div>
+    </div> -->
 
     <script src="http://code.jquery.com/jquery-latest.min.js"></script>
     <script src="scripts/model.js"></script>
--- scripts/controllers.js
+++ scripts/controllers.js
@@ -14,9 +14,15 @@ function swapMiddleView(type) {
 	if(type === 'search') {
 		$('#search').show();
 		$('#browse').hide();
+		$('#a2').css('background', '#FFB3AE');
+		$('#b2').css('background', '#FFB3AE');
+		$('#c2').css('background', '#FFB3AE');
 	}
 	else if(type === 'browse') {
 		$('#browse').show();
 		$('#search').hide();
+		$('#a2').css('background', '#68B287');
+		$('#b2').css('background', '#68B287');
+		$('#c2').css('background', '#68B287');
 	}
 }
