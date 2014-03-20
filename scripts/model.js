932b8a3fe3d2b57642e6b1db0d72781456a46e2c
bratzie <xumbik@gmail.com>
2014-03-21 00:52:41 +0100

    Collections slowly shaping up

--- scripts/model.js
+++ scripts/model.js
@@ -4,17 +4,27 @@
 Songs
 ***************************************/
 
-//This is the song constructor
-//If you want to create a new song call
-// var song = new Song("song title", "det här är en fin sång", "valfri", "snapsvisa")
-//The type is allowed to be null
-function Song(title, lyrics, melody, composer, type){
-	var _title = title; //String
-	var _lyrics = lyrics; //String
-	var _melody = melody; //String
-	var _composer = composer; //String
-	var _type = type; //String or enum (?)
+// This is the song constructor
+// If you want to create a new song call
+// var song = new Song(1, "song title", "det här är en fin sång", "valfri", "some dude", "snapsvisa")
+// The type is allowed to be null
+function Song(id, title, lyrics, melody, composer, type){
 
+	var _id = id; // int
+	var _title = title; // String
+	var _lyrics = lyrics; // String
+	var _melody = melody; // String
+	var _composer = composer; // String
+	var _type = type; // String or enum (?)
+
+	this.setId = function(id){
+		_id = id;
+		model.notifyObservers();
+	}
+
+	this.getId = function(){
+		return _id;
+	}
 
 	this.setTitle = function(title){
 		_title = title;
@@ -67,37 +77,46 @@ function Song(title, lyrics, melody, composer, type){
 Collections
 ***************************************/
 
-//This is the Collection constructor
-//If you want to create a new collection call
-// var collection = new Collection("My Collection","this is awesum", [], user.getName(), true)
-function Collection(name, description, songs, creator, isPublic) {
+// This is the Collection constructor
+// If you want to create a new collection call
+// var collection = new Collection(1, My Collection", "this is awesum", user.getId(), true)
+function Collection(id, name, description, creator, isPublic) {
 
 	/*
 	TODO: HAS TO BE REDONE TO BE ABLE TO ADD SONGS TO AN EXISTING COLLECTION
 	WE SHOULD PROBABLY PASS COLLECTION ID WITH COLLECTION FUNCTIONS ?
 	*/
 
-	var _name = name; //String
-	var _description = description; //String
-	var _songs = songs; //Array of Song
-	var _creator = creator; // String
+	var _id = id; // int
+	var _name = name; // String
+	var _description = description; // String
+	var _creator = creator; // int
 	var _isPublic = isPublic; // Boolean
 
-	this.setName = function(name){
+	this.setId = function(id) {
+		_id = id;
+		model.notifyObservers();
+	}
+
+	this.getId = function() {
+		return _id;
+	}
+
+	this.setName = function(name) {
 		_name = name;
 		model.notifyObservers();
 	}
 
-	this.getName = function(){
+	this.getName = function() {
 		return _name;
 	}
 
-	this.setDescription = function(description){
+	this.setDescription = function(description) {
 		_description = description;
 		model.notifyObservers();
 	}
 
-	this.getDescription = function(){
+	this.getDescription = function() {
 		return _description;
 	}
 
@@ -119,52 +138,61 @@ function Collection(name, description, songs, creator, isPublic) {
 		model.notifyObservers();
 	}
 
-	this.getSongs = function(){
+	this.getSongs = function() {
 		return _songs;
 	}
 
-	this.setCreator = function(creator){
+	this.setCreator = function(creator) {
 		_creator = creator;
 		model.notifyObservers();
 	}
 
-	this.getCreator = function(){
+	this.getCreator = function() {
 		return _creator;
 	}
 
-	this.setIsPublic = function(isPublic){
+	this.setIsPublic = function(isPublic) {
 		_isPublic = isPublic;
 		model.notifyObservers();
 	}
 
-	this.getIsPublic = function(){
+	this.getIsPublic = function() {
 		return _isPublic;
 	}
 
-	this.clear = function(){
+	this.clear = function() {
 		this.setName = "";
 		this.clearSongs();
 		this.setCreator = "";
 		this.setIsPublic = false;
 		model.notifyObservers();
 	}
-
 }
 
 /***************************************
 Users
 ***************************************/
 
-//This is the User constructor
-//If you want to create a new collection call
+// This is the User constructor
+// If you want to create a new collection call
 // var user = new User("Username", "Firstname", "Surname", "2014-03-14", "Password")
-function User(username, firstname, surname, joined, password) {
+function User(id, username, firstname, surname, joined, password) {
 
-	var _username = username; //String
-	var _firstname = firstname; //String
-	var _surname = surname; //String
-	var _joined = joined; //timestamp
+	var _id = id; // id
+	var _username = username; // String
+	var _firstname = firstname; // String
+	var _surname = surname; // String
+	var _joined = joined; // Timestamp
 	var _password = password; // String (so secure)
+
+	this.setId = function(id){
+		_id = id;
+		model.notifyObservers();
+	}
+
+	this.getId = function(){
+		return _id;
+	}
 
 	this.setUsername = function(username){
 		_username = username;
@@ -211,59 +239,66 @@ function User(username, firstname, surname, joined, password) {
 Model
 ***************************************/
 
-// this is the main model that holds information about alla songs,
+// this is the main model that holds information about all songs,
 // and collections as well as the collection that the user is currently
 // working on.
 
-function Model() {
+function Model () {
 	this.songs = [];
 	this.collections = [];
 	this.users = [];
-	this.workingCollection = new Collection("","", [], "", false);
 
-	this.addSong = function (title, lyrics, melody, composer, type){
+	this.addSong = function (id, title, lyrics, melody, composer, type){
 		var _melody;
 		var _composer;
-		if(!title){
+		if(!id || !title || !lyrics) {
 			return;
 		}
-		if(!lyrics){
-			return;
-		}
-		if(melody){
+		if(melody) {
 			_melody = melody;
-		}else{
-			_melody = "okänd";
+		} else {
+			_melody = "Okänd";
 		}
-		if(composer){
+		if(composer) {
 			_composer = composer;
 		} else {
-			_composer = "okänd";
+			_composer = "Okänd";
 		}
-		var song = new Song(title,lyrics,_melody,_composer,type);
+		var song = new Song(id, title, lyrics, _melody, _composer, type);
 		this.songs.push(song);
 		this.notifyObservers();
 	}
 
-	this.addCollection = function () {
-		if(!this.workingCollection.getName()) {
+	this.addCollection = function (id, name, description, creator, isPublic) {
+		var _description;
+		var _creator;
+		var _isPublic;
+		if(!id || !name) {
 			return;
 		}
-		if(!this.workingCollection.getSongs()) {
-			return;
-		}
-		this.collections.push(this.workingCollection);
-		this.workingCollection.clear;
+		_description = (description ? description : "");
+		_creator = (creator ? creator : 0);
+		_isPublic = (isPublic ? isPublic : true);
+
+		this.collections.push(collection);
 		this.notifyObservers();
 	}
 
-	this.addUser = function (username, firstname, surname, password) {
-		if(!username || !firstname || !surname || !password) { // all fields are required for users.
+	this.addSongToCollection = function (collectionid, songid) {
+
+	}
+
+	this.removeSongFromCollection = function (collectionid, songid) {
+		
+	}
+
+	this.addUser = function (id, username, firstname, surname, password) {
+		if(!id || !username || !firstname || !surname || !password) { // all fields are required for users.
 			return;
 		}
 		var now = new Date();
-		var newuser = new User(username, firstname, surname, now, password);
-		this.users.push(newuser);
+		var user = new User(id, username, firstname, surname, now, password);
+		this.users.push(user);
 		this.notifyObservers;
 	}
 
@@ -285,43 +320,40 @@ function Model() {
 // this is the instance of the main model
 var model = new Model();
 createTestData();
+
 // This method can be used to create some test data and test your implementation
 // This should not be used in the final version.
 function createTestData(){
-	model.workingCollection.setName("epic collection");
 
+	// import JSON and add songs to model
 	$.getJSON( "./db/songs.json", function( songs ) {
 		console.log(songs);
 		var i = 0;
-
 		while(songs.songs[i] != null) {
 			var song = songs.songs[i];
-			model.addSong(song.title, song.lyrics, song.melody, song.composer, song.type);
+			model.addSong(song.songid, song.title, song.lyrics, song.melody, song.composer, song.type);
 			i++;
+			console.log(song.songid + ": " + song.title + " added!");
 		}
 	});
 
 	$.getJSON( "./db/users.json", function( users ) {
 		console.log(users);
 		var i = 0;
-
 		while(users.users[i] != null) {
 			var user = users.users[i];
-			model.addUser(user.username, user.firstname, user.surname, user.password);
+			model.addUser(user.userid, user.username, user.firstname, user.surname, user.password);
 			i++;
+			console.log(user.userid + ": " + user.title + " added!");
 		}
 	});
-
-	model.workingCollection.addSong(model.songs[1]);
-	model.workingCollection.addSong(model.songs[0]);
-	model.addCollection();
 	
 	/*console.log("Song 2 is: " + model.songs[1].getTitle());
 	console.log("The lyrics of Song 1 is: " + model.songs[0].getLyrics());
 	console.log("Length of Collection 1 is: " + model.collections[0].getSongs().length);
 	console.log("This should be 0 and is: " + model.workingCollection.getSongs.length);*/
 
-	var songs = model.collections[0].getSongs();
+	//var songs = model.collections[0].getSongs();
 
 	/*console.log("");
 	console.log("Info om alla sånger som finns i den collectionen som skapats: ");
